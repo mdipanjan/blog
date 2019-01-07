cb3e2e38750cab9f4837f7f14f5bad7e40d70b8c
mdipanjan <mdipanjan33@gmail.com>
2019-01-07 02:00:12 -1200

    make it better

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports={
-        mongoURI:''
+        mongoURI:'mongodb://mdipanjan:mdipanjan1@ds151124.mlab.com:51124/blog'
     }
 }else module.exports={
     mongoURI:'mongodb://localhost/blogpage'
