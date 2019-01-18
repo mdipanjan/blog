8c63bde32b81c78f33c7593730324e6814cbd972
Dipanjan Mondal <38426196+mdipanjan@users.noreply.github.com>
2019-01-17 18:31:48 -1200

    Delete database.js

--- config/database.js
+++
@@ -1,7 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports={
-        mongoURI:'mongodb://mdipanjan:mdipanjan1@ds151124.mlab.com:51124/blog'
-    }
-}else module.exports={
-    mongoURI:'mongodb://localhost/blogpage'
-}
